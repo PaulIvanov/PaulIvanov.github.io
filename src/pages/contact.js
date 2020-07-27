4d64ad7d44fe72e1c6c32f7fb9e42d59e50658d6
PaulIvanov <paulivanov123@gmail.com>
2020-07-26 20:06:46 -0700

    contact case sensitivity

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
-import styles from './Contact.module.css'
+import styles from './contact.module.css'
 import Layout from '../components/layout'
 import ContactForm from '../components/contact-form'
 
